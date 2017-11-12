cd19b9053118e4a6b033683bb0505e4c7925d9f0
Rais Mohamed <Rais Mohamed>
2017-11-12 20:00:59 +0100

    first push

---
+++ src/main/java/com/mn/mncab/MncabApplication.java
@@ -0,0 +1,13 @@
+package com.mn.mncab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MncabApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(MncabApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mn/mncab/MncabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mn.mncab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MncabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
